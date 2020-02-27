917c2dcef506cf0e26d80516db181291bbca895d
Ethan Allis <54921212+ethanallis@users.noreply.github.com>
2020-02-26 23:37:51 -0500

    Update secure-ssh.sh

--- SYS265/centos7/secure-ssh.sh
+++ SYS265/centos7/secure-ssh.sh
@@ -6,6 +6,7 @@
 echo "Creating new no-password SSH user!"
 read -p "Enter name of new user: " x
 sudo useradd -m -d /home/$x -s /bin/bash $x
+mkdir -p /home/$x/.ssh/authorized_keys
 sudo cp -rv /root/ethanstechjournal/SYS265/public-keys/id_rsa.pub /home/$x/.ssh/authorized_keys
 sudo chmod 700 /home/$x/.ssh
 sudo chmod 600 /home/$x/.ssh/authorized_keys
